7f63b22b1dc3009a374452eec4be7e5831a220d9
Almuhannad <almahanad1@gmail.com>
2024-04-06 14:53:55 +0400

    build modal for display details about pokemon and style of it.

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import PokemonDetails from './PokemonDetails';
 
 function PokemonCard({ url }) {
     const [info, setInfo] = useState(null);
@@ -16,13 +17,18 @@ function PokemonCard({ url }) {
     }, [url]);
 
     if (!info) return <div class="loader"></div>;
+
+    function capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
     return (
         <div className={`${info.types[0].type.name} thumb-container`}>
             <div className="number"><small>#0{info.id}</small></div>
             <img src={info.sprites.front_default} alt={info.name} />
             <div className="detail-wrapper">
-                <h3>{info.name}</h3>
+                <h3>{capitalize(info.name)}</h3>
                 <small>Type: {info.types[0].type.name}</small>
+                <PokemonDetails details={info} />
             </div>
         </div>
     );
--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,8 +1,96 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./Modal.css";
 
-function PokemonDetails() {
+function PokemonDetails({ details }) {
+  const [modal, setModal] = useState(false);
+  const [evolution, seEvolution] = useState(null);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   return (
-    <div>PokemonDetails</div>
+    <>
+      <button onClick={toggleModal} className="btn-modal">
+        more
+      </button>
+
+      {modal && (
+        <div className="modal" key={details.id}>
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <h2>{capitalize(details.name)}</h2>
+            <div className="container-details">
+              <div className="left">
+                <img src={details.sprites.front_default} alt={details.name} style={{ width: "25%", height: "40%" }} />
+                <div>
+
+
+
+                </div>
+              </div>
+
+              <div className="right">
+                <div className="table-container">
+                  <table class="top-table">
+                    <tr>
+                      <th>Type</th>
+                      <td>{capitalize(details.types[0].type.name)}</td>
+                    </tr>
+
+                    <tr>
+                      <th>Hight</th>
+                      <td>{details.height} Cm</td>
+                    </tr>
+
+                    <tr>
+                      <th>Weight</th>
+                      <td>{details.weight} Lbs</td>
+                    </tr>
+                  </table>
+
+                  <table className="bottom-table">
+                    <tr>
+                      <th>Hp</th>
+                      <td>{details.stats[0].base_stat}</td>
+                    </tr>
+
+                    <tr>
+                      <th>Attack</th>
+                      <td>{details.stats[1].base_stat}</td>
+                    </tr>
+
+                    <tr>
+                      <th>Defense</th>
+                      <td>{details.stats[2].base_stat}</td>
+                    </tr>
+
+                    <tr>
+                      <th>Speed</th>
+                      <td>{details.stats[5].base_stat}</td>
+                    </tr>
+                  </table>
+                </div>
+              </div>
+            </div>
+
+            <button className="close-modal" onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
